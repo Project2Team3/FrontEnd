7f45846d9a37e92e5ac4f9c5f59ba74360abb03e
Sam <hengliew4@gmail.com>
2022-07-15 13:40:35 -0400

    Friends Component work

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< Updated upstream
-=======
 import { AppComponent } from '../../app.component';
->>>>>>> Stashed changes
 
 @Component({
   selector: 'app-navigation',
@@ -10,17 +7,13 @@ import { AppComponent } from '../../app.component';
   styleUrls: ['./navigation.component.css'],
 })
 export class NavigationComponent implements OnInit {
-<<<<<<< Updated upstream
 
-  constructor() { }
-
-  ngOnInit(): void {
-=======
   constructor(private appComponent: AppComponent) {}
 
+  ngOnInit(): void {
+
+  }
   signOut(): void {
     this.appComponent.signOut();
->>>>>>> Stashed changes
   }
-  ngOnInit(): void {}
 }
