499e03c618a0c5635e3f5b3cf3e6c9b1b37e207a
davidfz370 <106416414+davidfz370@users.noreply.github.com>
2022-07-20 10:52:07 -0700

    password validation

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,3 +1,4 @@
+import { LoginService } from './../../services/loginService/login.service';
 import { AppComponent } from './../../app.component';
 import { UserService } from 'src/app/services/userService/users.service';
 import { Component, OnInit } from '@angular/core';
@@ -10,6 +11,7 @@ import {User} from "../../models/user";
   styleUrls: ['./profile-page.component.css']
 })
 export class ProfilePageComponent implements OnInit {
+<<<<<<< Updated upstream
 
   constructor(private userService:UserService, private appComponent:AppComponent){
 
@@ -23,6 +25,22 @@ export class ProfilePageComponent implements OnInit {
 
   ngOnInit() :void{
     if (!sessionStorage.getItem("user")) {
+=======
+  constructor(
+    private userService: UserService,
+    private appComponent: AppComponent,
+    private router:Router,
+    private loginService: LoginService
+  ) {}
+
+  user: User = new User(0, '', '', '', 0, '');
+  userString: string | null = sessionStorage.getItem('user');
+  active:string = "title";
+  notif:string = 'Error';
+
+  ngOnInit(): void {
+    if (!sessionStorage.getItem('user')) {
+>>>>>>> Stashed changes
       this.appComponent.signOut();
       return;
     }
@@ -36,9 +54,50 @@ export class ProfilePageComponent implements OnInit {
     this.userService.findUserById(userJSON.id).subscribe({
       next:(data)=>{
         this.user = data;
+<<<<<<< Updated upstream
         console.log(this.user);
       }
     })
+=======
+      },
+    });
+  }
+
+  onChangePasswordListItemClick() {
+    this.active = this.active === "form" ? "title" : "form";
+  }
+
+  changePassword(event:any):void{
+
+
+    event.stopPropagation();
+
+
+    if(this.user.password.trim().length==0){
+
+
+      this.notif = 'INVALID';
+      setTimeout(() => (this.notif = 'Changed'), 3000);
+      return;
+
+  }
+
+    this.userService.updateUser(this.user).subscribe({
+
+
+      next: () => {
+
+
+
+
+      }
+    })
+    this.router.navigate(['/home']);
+
+
+
+  }
+>>>>>>> Stashed changes
 
     // this.user= sessionStorage.getItem('user');
 
